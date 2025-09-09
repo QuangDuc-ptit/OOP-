4f4413ceffbb67eabd93b1d56017316b11c1b76a
QuangDuc-ptit <Ducqn.ptit@gmail.com>
2025-09-09 17:32:21 +0700

    Add files via upload
    Bài tập trên lớp chiều 9/9/2025

---
+++ 9_9_2025/Main1.java
@@ -0,0 +1,35 @@
+pa
+class HinhChuNhat {
+    private double chieuDai;
+    private double chieuRong;
+
+    public HinhChuNhat(double cd, double cr) {
+        this.chieuDai = cd;
+        this.chieuRong = cr;
+    }
+
+    public double chuVi() {
+        return 2 * (chieuDai + chieuRong);
+    }
+
+    public double dienTich() {
+        return chieuDai * chieuRong;
+    }
+
+    public void hienThi() {
+        System.out.println("Hinh chu nhat: CD=" + chieuDai +
+                           ", CR=" + chieuRong +
+                           ", Chu vi=" + chuVi() +
+                           ", Dien tich=" + dienTich());
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        HinhChuNhat h1 = new HinhChuNhat(5, 3);
+        HinhChuNhat h2 = new HinhChuNhat(10, 4);
+
+        h1.hienThi();
+        h2.hienThi();
+    }
+}
